f0de458bd48b22c97b2bd8f22f8b1d034e5675f6
N <n@itsacomputer.local>
2020-06-09 15:05:40 -0700

    add output_data_dir path

--- podi/conf.py
+++ podi/conf.py
@@ -2,3 +2,5 @@ from pathlib import Path
 
 
 DATA_DIR = Path(__file__).resolve(strict=True).parent.parent / "data"
+
+OUTPUT_DATA_DIR = Path(__file__).resolve(strict=True).parent.parent / "output_data"
