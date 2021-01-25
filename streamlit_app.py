f2fa17d49e542d59fef4563e3d7e7c6b1a5ea4fd
na <nadams@epicinstitute.org>
2021-01-25 08:42:18 -0800

    streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,12 +7,17 @@ st.title("Positive Disruption")
 
 energy_demand = pd.read_csv("energy_demand_out.csv")
 
-st.subheader("Energy Demand")
-st.area_chart(energy_demand)
+# https://pandas.pydata.org/docs/user_guide/10min.html
+energy_demand.stack()
 
 c = alt.Chart(energy_demand).mark_area().encode(x="", y="gen", color="Sector")
 
 st.altair_chart(c)
 
+"""
 st.subheader("Energy Demand")
 st.line_chart(energy_demand)
+
+st.subheader("Energy Demand")
+st.area_chart(energy_demand)
+"""
