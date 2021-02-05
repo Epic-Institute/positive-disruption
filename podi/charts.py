7aa60301122503d958cf3e6973d39b75a1e5c47b
nick-epicinstitute <nadams@epicinstitute.org>
2021-02-05 10:42:05 -0800

    charts

--- podi/charts.py
+++ podi/charts.py
@@ -1315,7 +1315,7 @@ for i in range(0, len(iea_region_list)):
             y="Emissions, GtCO2e",
             line_group="Sector",
             color="Sector",
-            color_discrete_sequence=["#EDEDED", "#FF9DA6", "#E45756", "#72B7B2", "#54A24B", "#9D755D", "#F58518", "#EECA3B", "#B279A2"],
+            color_discrete_sequence=["#EDEDED", "#FF9DA6", "#E45756", "#72B7B2", "#54A24B", "#60738C", "#F58518", "#7AA8B8", "#B279A2"],
             title="Emissions Mitigated, " + iea_region_list[i],
             hover_data={"Emissions, GtCO2e": ":.0f"}, category_orders={'Sector': [spacer.name, 'CDR', 'CH4, N2O, F-gases', 'Forests & Wetlands', 'Agriculture', 'Industry', 'Buildings', 'Transport', 'Electricity']}
         )
