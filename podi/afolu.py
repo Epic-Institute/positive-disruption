6a0eccdca25a67e82ffeb003bf3cb3c006c3541c
nick-epicinstitute <nadams@epicinstitute.org>
2021-07-01 17:04:53 -0700

    afolu

--- podi/afolu.py
+++ podi/afolu.py
@@ -479,7 +479,7 @@ def afolu(scenario):
         axis=1,
     )
 
-    avoid_per = avoid.apply(lambda x: ((x[2019] - x) / x.max()).fillna(0), axis=1)
+    avoid_per = -avoid.apply(lambda x: ((x[2019] - x) / x.max()).fillna(0), axis=1)
 
     """
     avoid_per = avoid.apply(lambda x: ((x[2019] - x) / x[2019]).fillna(0), axis=1)
